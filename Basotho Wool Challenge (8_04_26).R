43d74be6fd918152b61bc9cfbeef5a30704abd37
tesshinkle <isabellajudehinkle@gmail.com>
2026-08-02 16:00:12 -0500

    Start of Wool Challenge

---
+++ Basotho Wool Challenge (8_04_26).R
@@ -0,0 +1,10 @@
+#Tidy Tuesday CHallenge 8/4/2026
+#Basotho Wool
+# Challenge Questions:
+
+##Does Winter (being in the Southern hemisphere, these are the months June, July and August) 
+#impact Basotho wool quantity/volume exported and revenue?
+
+##Which country imports the most Basotho wool at time on average?
+
+basotho_wool = readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-08-04/basotho_wool.csv')
